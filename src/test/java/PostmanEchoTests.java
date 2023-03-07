6f2c7654846d78d632a68802eb0b7c4d5c9da717
Victor Balandin <eparis@atlassian.com>
2023-03-07 10:21:02 +0300

    Failed Postman test

--- src/test/java/PostmanEchoTests.java
+++ src/test/java/PostmanEchoTests.java
@@ -1,13 +1,11 @@
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTests {
     @Test
-
-     void shouldTestPostmanEcho (){
+    void shouldTestPostmanEcho() {
 // Given - When - Then
 // Предусловия
         given()
@@ -19,7 +17,7 @@ public class PostmanEchoTests {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("01.01.2023"))
+                .body("data", equalTo("01.02.2023"))
         ;
     }
 }
