4cb49726439c4846cce28440e5e67e010aba1622
Victor Balandin <eparis@atlassian.com>
2023-03-07 10:17:15 +0300

    Success Postman test

---
+++ src/test/java/PostmanEchoTests.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTests {
+    @Test
+
+     void shouldTestPostmanEcho (){
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("01.01.2023") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("01.01.2023"))
+        ;
+    }
+}
